66124a11f6b585710f43ff0499f530a757098a7f
latinstallion <156602623+latinstallion@users.noreply.github.com>
2021-04-25 15:19:45 +0000

    Create seedbot.lua

--- src/main/java/rainbownlp/analyzer/evaluation/FeatureEvaluator.java
+++ src/main/java/rainbownlp/analyzer/evaluation/FeatureEvaluator.java
@@ -1,4 +1,12 @@
 package rainbownlp.analyzer.evaluation;
 
 import java.util.ArrayList;
-import java.util.HashMa
+import java.util.HashMap;
+import java.util.List;
+
+import rainbownlp.core.FeatureValuePair;
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.machinelearning.convertor.SVMLightFormatConvertor;
+
+public class FeatureEvaluator {
+	public void evaluateFeatures(ICrossfoldValidator cfV
