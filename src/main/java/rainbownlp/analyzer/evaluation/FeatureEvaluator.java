4c226082e0137f6d177571845081dfb2eed7240d
latinstallion <156602623+latinstallion@users.noreply.github.com>
2021-04-26 08:17:22 +0000

    git

--- src/main/java/rainbownlp/analyzer/evaluation/FeatureEvaluator.java
+++ src/main/java/rainbownlp/analyzer/evaluation/FeatureEvaluator.java
@@ -9,4 +9,8 @@ import rainbownlp.machinelearning.MLExample;
 import rainbownlp.machinelearning.convertor.SVMLightFormatConvertor;
 
 public class FeatureEvaluator {
-	public void evaluateFeatures(ICrossfoldValidator cfV
+	public void evaluateFeatures(ICrossfoldValidator cfValidator, List<MLExample> examples) throws Exception{
+		SVMLightFormatConvertor.onlyIncludeAttributes = new ArrayList<String>();
+		List<String> features = FeatureValuePair.getAllFeatureNames();
+		String featuresIncluded = "";
+		HashMap<String, IEvaluationResult> attributeResult
