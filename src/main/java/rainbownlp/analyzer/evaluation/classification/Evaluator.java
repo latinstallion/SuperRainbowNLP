cec5923fd544e0d8c12d509a1dbc6c25e361ee57
latinstallion <156602623+latinstallion@users.noreply.github.com>
2021-05-05 15:45:37 +0000

    Fix broken link in README

--- src/main/java/rainbownlp/analyzer/evaluation/classification/Evaluator.java
+++ src/main/java/rainbownlp/analyzer/evaluation/classification/Evaluator.java
@@ -1,1 +1,11 @@
-package rainbownlp.analyzer.eva
+package rainbownlp.analyzer.evaluation.classification;
+
+import java.util.ArrayList;
+import java.util.List;
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.util.SystemUtil;
+
+
+public class Evaluator {
+	public static boolean saveResult = false;
+	public static String
