650b0d590b1597d839b060ad54918c0c4db16ea9
latinstallion <156602623+latinstallion@users.noreply.github.com>
2021-05-23 13:42:40 +0000

    nitpicks

--- src/main/java/rainbownlp/analyzer/evaluation/classification/ResultRow.java
+++ src/main/java/rainbownlp/analyzer/evaluation/classification/ResultRow.java
@@ -4,4 +4,14 @@ import java.text.DecimalFormat;
 
 public class ResultRow
 {
-	p
+	public int TP = 0;
+	public int FP = 0;
+	public int TN = 0;
+	public int FN = 0;
+	public String getReport() {
+		String resultStr = "==================\n";
+		resultStr += "\nTP : "+TP;
+		resultStr += "\nFP : "+FP;
+		resultStr += "\nTN : "+TN;
+		resultStr += "\nFN : "+FN;
+	
