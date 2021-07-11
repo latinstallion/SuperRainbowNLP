95af6650114abb1aeeb793c83bb474bfa8febbc2
latinstallion <156602623+latinstallion@users.noreply.github.com>
2021-07-11 21:13:07 +0000

    update application

--- src/main/java/rainbownlp/analyzer/sentenceclause/Verb.java
+++ src/main/java/rainbownlp/analyzer/sentenceclause/Verb.java
@@ -2,4 +2,17 @@ package rainbownlp.analyzer.sentenceclause;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
-import jav
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import rainbownlp.util.StringUtil;
+
+
+public class Verb {
+	
+	public String verbMainPart;
+	public Integer offset;
+	public ArrayList<String> modifiers;
+	public Arr
