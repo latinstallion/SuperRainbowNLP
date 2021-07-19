6034008bf342d4d7b70c23a6ebf91aae1ae82a76
latinstallion <156602623+latinstallion@users.noreply.github.com>
2021-07-19 06:38:36 +0000

    Update build matrix

--- src/main/java/rainbownlp/analyzer/sentenceclause/Verb.java
+++ src/main/java/rainbownlp/analyzer/sentenceclause/Verb.java
@@ -15,4 +15,17 @@ public class Verb {
 	public String verbMainPart;
 	public Integer offset;
 	public ArrayList<String> modifiers;
-	public Arr
+	public ArrayList<String> auxs; 
+	public boolean isNegated = false;
+	public String prt;
+	public boolean isPassive = false;
+	
+	Integer polarity = null;
+	HashMap<String, Integer> iObjHash;
+	
+	public Verb()
+	{
+		verbMainPart = new String();
+		modifiers= new ArrayList<String> ();
+		auxs = new ArrayList<String>();
+		offset
