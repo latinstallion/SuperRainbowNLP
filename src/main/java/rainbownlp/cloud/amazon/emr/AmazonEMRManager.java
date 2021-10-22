5c6d00b310cd90fb783928bb51b3ea27aa380861
latinstallion <156602623+latinstallion@users.noreply.github.com>
2021-10-22 15:40:27 +0000

    improve Makefile

--- src/main/java/rainbownlp/cloud/amazon/emr/AmazonEMRManager.java
+++ src/main/java/rainbownlp/cloud/amazon/emr/AmazonEMRManager.java
@@ -10,4 +10,9 @@ import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
 import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClient;
 import com.amazonaws.services.elasticmapreduce.model.AddJobFlowStepsRequest;
 import com.amazonaws.services.elasticmapreduce.model.AddJobFlowStepsResult;
-import com.amazona
+import com.amazonaws.services.elasticmapreduce.model.HadoopJarStepConfig;
+import com.amazonaws.services.elasticmapreduce.model.StepConfig;
+import com.amazonaws.services.elasticmapreduce.util.StepFactory;
+
+public class AmazonEMRManager {
+	public void runOnEMR(List<HadoopJarStepConfig>
