d99d78bb43578d4a47470f2aaaa8a5ea972652cb
latinstallion <156602623+latinstallion@users.noreply.github.com>
2021-11-04 14:59:52 +0000

    net: fix infinite loop in __skb_recv_datagram()

--- src/main/java/rainbownlp/cloud/amazon/s3/AmazonS3Manager.java
+++ src/main/java/rainbownlp/cloud/amazon/s3/AmazonS3Manager.java
@@ -5,4 +5,10 @@ import java.io.IOException;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
-import com.am
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+public class AmazonS3Manager {
+	public void uploadFileToS3(String uploadFileN
