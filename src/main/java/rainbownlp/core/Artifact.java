107a18b5906354ee8cace1dc79d54fe1bdd16ba0
latinstallion <156602623+latinstallion@users.noreply.github.com>
2021-11-17 21:13:26 +0000

    Add more info to readme

--- src/main/java/rainbownlp/core/Artifact.java
+++ src/main/java/rainbownlp/core/Artifact.java
@@ -484,3 +484,283 @@ public class Artifact {
 		if(!(pArtifact instanceof Artifact))
 			return false;
 		Artifact a = (Artifact)pArtifact;
+		return (a.getArtifactId() == artifactId) ||
+				(a.getStartIndex() == getStartIndex() &&
+				a.getContent().equals(getContent()) &&
+				a.getAssociatedFilePath().equals(getAssociatedFilePath()) &&
+				a.getArtifactType().equals(getArtifactType()));
+	}
+	@Override public int hashCode()
+	{
+		return artifactId;
+	}
+	
+	public static Artifact findInstance(Type pArtifactType, String pFilePath, 
+			Integer pWordIndex, Integer pLineIndex){
+		String hql = "from Artifact where artifactType =:artifactType  and" +
+				" associatedFilePath like :associatedFilePath and wordIndex=:wordIndex " +
+				" and lineIndex= :lineIndex";
+		
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("associatedFilePath", '%'+pFilePath);
+		params.put("wordIndex", pWordIndex);
+		params.put("artifactType", pArtifactType.ordinal());
+		params.put("lineIndex",pLineIndex);
+		
+		
+		List<Artifact> artifact_objects = 
+				(List<Artifact>) HibernateUtil.executeReader(hql,params);
+	    
+	    
+	    Artifact artifact_obj=null;
+	    if(artifact_objects.size()!=0)
+	    {
+	    	artifact_obj = 
+				artifact_objects.get(0);
+	    }
+	    return artifact_obj;
+	}
+	
+	
+	/**
+	 * Find an artifact inside sentence at given offset
+	 * @param pSentence
+	 * @param pWordIndex
+	 * @return
+	 */
+	public static Artifact findInstance(Artifact pSentence,int pWordIndex){
+		String hql = "from Artifact where parentArtifact = "+pSentence.getArtifactId()+" and wordIndex =" +
+		pWordIndex;
+		
+		List<Artifact> artifact_objects = 
+				(List<Artifact>) HibernateUtil.executeReader(hql);
+	    
+	    
+	    Artifact artifact_obj=null;
+	    if(artifact_objects.size()!=0)
+	    {
+	    	artifact_obj = 
+				artifact_objects.get(0);
+	    }
+	    return artifact_obj;
+	}
+	public static Artifact findInstance(Type pArtifactType, String pFilePath, 
+			int pLine, int pWordIndex, String pContent, String pSecondWord){
+		
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		String hql = "from Artifact where artifactType =:artifactType  and" +
+				" associatedFilePath like :associatedFilePath and content like:content " +
+				" and lineIndex= :lineIndex ";
+		if (pSecondWord != null)
+		{
+			hql += "and nextArtifact.content = :nextContent";
+			params.put("nextContent", pSecondWord);
+		}
+		else
+		{
+			hql += " and ABS(wordIndex - :pWordIndex) <=5";
+			params.put("pWordIndex",pWordIndex);
+		}
+		
+		params.put("associatedFilePath", '%'+pFilePath);
+		params.put("content", pContent+'%');
+		params.put("artifactType", pArtifactType.ordinal());
+		params.put("lineIndex",pLine);
+		
+		
+		
+		List<Artifact> artifact_objects = 
+				(List<Artifact>) HibernateUtil.executeReader(hql,params);
+	    
+	    
+	    Artifact artifact_obj=null;
+	    if(artifact_objects.size()!=0)
+	    {
+	    	artifact_obj = 
+				artifact_objects.get(0);
+	    }
+	    return artifact_obj;
+	}
+	public static Artifact findInstance(Type pArtifactType, String pFilePath, 
+			int pStartChar, String pContent, String pSecondWord){
+		
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		String hql = "from Artifact where artifactType =:artifactType  and" +
+				" associatedFilePath like :associatedFilePath and content like:content " +
+				" and ABS(startIndex - :pStartIndex) <45";
+		if (pSecondWord != null)
+		{
+			hql += "and nextArtifact.content = :nextContent";
+			params.put("nextContent", pSecondWord);
+		}
+	
+		
+		params.put("associatedFilePath", '%'+pFilePath);
+		params.put("content", pContent+'%');
+		params.put("artifactType", pArtifactType.ordinal());
+		params.put("pStartIndex",pStartChar);
+		
+		
+		
+		List<Artifact> artifact_objects = 
+				(List<Artifact>) HibernateUtil.executeReader(hql,params);
+	    
+	    
+	    Artifact artifact_obj=null;
+	    if(artifact_objects.size()!=0)
+	    {
+	    	artifact_obj = 
+				artifact_objects.get(0);
+	    }
+	    return artifact_obj;
+	}
+	public static Artifact findInstanceByStartIndex(String pFilePath,int pStartIndex, Type pArtifactType){
+		String hql = "from Artifact where associatedFilePath like :associatedFilePath  and  artifactType=:artifactType and (startIndex=:startIndex OR startIndex=:startIndex+1)";
+
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("associatedFilePath", '%'+pFilePath);
+		params.put("startIndex", pStartIndex);
+		params.put("artifactType", pArtifactType.ordinal());
+		
+
+		List<Artifact> artifact_objects = 
+					(List<Artifact>) HibernateUtil.executeReader(hql, params);
+
+
+	    Artifact artifact_obj=null;
+	    if(artifact_objects.size()!=0)
+	    {
+	    	artifact_obj = 
+				artifact_objects.get(0);
+	    }
+	    return artifact_obj;
+	}
+	public static List<Artifact> listByType(Type pArtifactType) {
+		String hql = "from Artifact where artifactType = "+pArtifactType.ordinal()
+				+" order by associatedFilePath";
+			
+		List<Artifact> artifact_objects = 
+				(List<Artifact>) HibernateUtil.executeReader(hql);
+		return artifact_objects;
+	}
+	public static List<Artifact> listByType(Type pSentenceType,boolean for_train) {
+		String file_path_pattern = "/train/";
+		if (!for_train)
+		{
+			file_path_pattern = "/test/";
+		}
+		String hql = "from Artifact where artifactType = "+pSentenceType.ordinal()
+				+"  and associatedFilePath like :pPattern order by associatedFilePath";
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("pPattern", '%'+file_path_pattern+'%');
+		List<Artifact> artifact_objects = 
+				(List<Artifact>) HibernateUtil.executeReader(hql,params);
+		return artifact_objects;
+	}
+
+	public void setStanDependency(String stanDependency) {
+		this.stanDependency = stanDependency;
+	}
+	public String getStanDependency() {
+		return stanDependency;
+	}
+	public void setStanPennTree(String stanPennTree) {
+		this.stanPennTree = stanPennTree;
+	}
+	public String getStanPennTree() {
+		return stanPennTree;
+	}
+	@Transient
+	List<Artifact> children = null;
+	@Transient
+	public Artifact getChildByWordIndex(int word_index) {
+		if(children==null)
+			loadChildren();
+		Artifact foundArtifact = null;
+		for(Artifact art : children)
+			if(art.getWordIndex() == word_index)
+			{
+				foundArtifact = art;
+				break;
+			}
+		return foundArtifact;
+	}
+	private void loadChildren() {
+		String hql = "from Artifact where parentArtifact = "+getArtifactId();
+		
+		children = 
+				(List<Artifact>) HibernateUtil.executeReader(hql);
+	}
+	
+
+	/**
+	 * Find document artifact by filename
+	 * @param fileName
+	 * @return
+	 */
+	
+	public static Artifact findDocument(String fileName) {
+		String hql = "from Artifact where associatedFilePath like :associatedFilePath  and  artifactType=:artifactType ";
+		
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("associatedFilePath", '%'+fileName);
+		params.put("artifactType", Type.Document.ordinal());
+		
+
+		List<Artifact> artifact_objects = 
+					(List<Artifact>) HibernateUtil.executeReader(hql, params);
+
+
+	    Artifact artifact_obj=null;
+	    if(artifact_objects.size()!=0)
+	    {
+	    	artifact_obj = 
+				artifact_objects.get(0);
+	    }
+	    return artifact_obj;
+	}
+	public String getArtifactOptionalCategory() {
+		return artifactOptionalCategory;
+	}
+	public void setArtifactOptionalCategory(String artifactOptionalCategory) {
+		this.artifactOptionalCategory = artifactOptionalCategory;
+	}
+	@Transient
+	public static List<Artifact> search(String query) {
+		String hql = "from Artifact where "+query;
+		
+		List<Artifact> artifact_objects = 
+					(List<Artifact>) HibernateUtil.executeReader(hql);
+
+	    return artifact_objects;
+	}
+	/**
+	 * Search by type and optional category and artifact filepath
+	 * @param passage
+	 * @param optionalCategory
+	 * @return
+	 */
+	public static List<Artifact> listByType(Type pArtifactType, String optionalCategory, String filePath) {
+		String hql = "from Artifact where artifactType = "+pArtifactType.ordinal()
+				+" and artifactOptionalCategory='"+optionalCategory+"' and associatedFilePath='"+filePath.replace("\\", "\\\\")+"'";
+			
+		List<Artifact> artifact_objects = 
+				(List<Artifact>) HibernateUtil.executeReader(hql);
+		return artifact_objects;
+	}
+	
+	/**
+	 * Search by type and optional category
+	 * @param passage
+	 * @param optionalCategory
+	 * @return
+	 */
+	public static List<Artifact> listByType(Type pArtifactType, String optionalCategory) {
+		String hql = "from Artifact where artifactType = "+pArtifactType.ordinal()
+				+" and artifactOptionalCategory='"+optionalCategory+"'";
+			
+		List<Artifact> artifact_objects = 
+				(List<Artifact>) HibernateUtil.executeReader(hql);
+		return artifact_objects;
+	}
+	
