ebf724341140c74231996c436da3eafed50ae5c4
latinstallion <156602623+latinstallion@users.noreply.github.com>
2021-11-24 22:40:34 +0000

    0.12.3

--- src/main/java/rainbownlp/core/Phrase.java
+++ src/main/java/rainbownlp/core/Phrase.java
@@ -3,4 +3,13 @@ package rainbownlp.core;
 import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.ArrayList;
-imp
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.per
