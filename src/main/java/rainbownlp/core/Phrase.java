a667cf62d42cc5bb831d29fdb482f622f5f2ce98
latinstallion <156602623+latinstallion@users.noreply.github.com>
2021-12-02 05:58:20 +0000

    Update torviet-shoutbox-enhancer.user.js

--- src/main/java/rainbownlp/core/Phrase.java
+++ src/main/java/rainbownlp/core/Phrase.java
@@ -12,4 +12,11 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.per
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.p
