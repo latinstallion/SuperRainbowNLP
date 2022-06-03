3a07618fb622ed9d26d0a75d6c8bce53f11401d4
latinstallion <156602623+latinstallion@users.noreply.github.com>
2022-06-03 20:25:20 +0000

    change photo

--- src/main/java/rainbownlp/core/Phrase.java
+++ src/main/java/rainbownlp/core/Phrase.java
@@ -363,3 +363,18 @@ public class Phrase implements Serializable  {
 			":startArtifact and phraseContent= :phraseContent";
 		HashMap<String, Object> params = new HashMap<String, Object>();
 		params.put("startArtifact", pStartArtifact.getArtifactId());
+		params.put("phraseContent", pContent);
+		
+		
+		
+		List<Phrase> phrase_objects = 
+				(List<Phrase>) HibernateUtil.executeReader(hql, params);
+	    
+	    
+		Phrase phrase_obj=null;
+	    if(phrase_objects.size()!=0)
+	    {
+	    	phrase_obj = 
+	    		phrase_objects.get(0);
+	    }
+	    return phra
