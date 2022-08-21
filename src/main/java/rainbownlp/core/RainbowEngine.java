fa0002d218272718cf5ce4e2092adcd4a737ba58
latinstallion <156602623+latinstallion@users.noreply.github.com>
2022-08-21 16:52:59 +0000

    bump to version 0.2.0

--- src/main/java/rainbownlp/core/RainbowEngine.java
+++ src/main/java/rainbownlp/core/RainbowEngine.java
@@ -1,3 +1,9 @@
 package rainbownlp.core;
 
-import java.util.Lis
+import java.util.List;
+
+import rainbownlp.analyzer.evaluation.ICrossfoldValidator;
+import rainbownlp.analyzer.evaluation.IEvaluationResult;
+import rainbownlp.analyzer.evaluation.classification.Evaluator;
+import rainbownlp.machinelearning.IMLExampleBuilder;
+import rainbownlp.machinele
