972ee70f6a8553032d016a1623ee0e0a621e9ca2
latinstallion <156602623+latinstallion@users.noreply.github.com>
2022-08-21 16:57:01 +0000

    options

--- src/main/java/rainbownlp/core/RainbowEngine.java
+++ src/main/java/rainbownlp/core/RainbowEngine.java
@@ -6,4 +6,14 @@ import rainbownlp.analyzer.evaluation.ICrossfoldValidator;
 import rainbownlp.analyzer.evaluation.IEvaluationResult;
 import rainbownlp.analyzer.evaluation.classification.Evaluator;
 import rainbownlp.machinelearning.IMLExampleBuilder;
-import rainbownlp.machinele
+import rainbownlp.machinelearning.LearnerEngine;
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.preprocess.DocumentAnalyzer.InputType;
+import rainbownlp.preprocess.SimpleDocumentLoader;
+
+/**
+ * This includes several sugar methods to make using RNLP easier 
+ * @author eemadzadeh
+ *
+ */
+public 
