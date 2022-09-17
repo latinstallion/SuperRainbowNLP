4cc03422828390a609997e4697bfd99c7b6ef4d9
latinstallion <156602623+latinstallion@users.noreply.github.com>
2022-09-17 11:13:02 +0000

    Controllers

--- src/main/java/rainbownlp/core/TimeStamp.java
+++ src/main/java/rainbownlp/core/TimeStamp.java
@@ -4,4 +4,12 @@ import java.util.Date;
 import java.util.HashMap;
 
 public class TimeStamp {
-	static HashMap<String, Date>
+	static HashMap<String, Date> blockTimes = new HashMap<String, Date>();
+	public static void setStart(String blockName) {
+		blockTimes.put(blockName, new Date());
+	}
+
+	public static void setEnd(String blockName) {
+		Date now = new Date();
+		Date start = blockTimes.get(blockName);
+		if(start
