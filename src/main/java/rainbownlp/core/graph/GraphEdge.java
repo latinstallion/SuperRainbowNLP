ab883dc4eb22f524ada44649812c9ffb632574c1
latinstallion <156602623+latinstallion@users.noreply.github.com>
2022-09-17 16:46:01 +0000

    ALSA: asihpi: Fix unlocked snd_pcm_stop() call

--- src/main/java/rainbownlp/core/graph/GraphEdge.java
+++ src/main/java/rainbownlp/core/graph/GraphEdge.java
@@ -1,3 +1,18 @@
 package rainbownlp.core.graph;
 
-import org.jgrapht.graph.DefaultE
+import org.jgrapht.graph.DefaultEdge;
+
+public class GraphEdge extends DefaultEdge {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public GraphEdge()
+	{
+		super();
+	}
+	
+    //~ Methods ----------------------------------------------------------------
+
+//	@Override
+	publi
