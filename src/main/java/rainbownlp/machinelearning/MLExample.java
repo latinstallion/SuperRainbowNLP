18049655e079aaaf415507de79fc64edcd48029a
latinstallion <156602623+latinstallion@users.noreply.github.com>
2022-10-05 22:39:44 +0000

    gs

--- src/main/java/rainbownlp/machinelearning/MLExample.java
+++ src/main/java/rainbownlp/machinelearning/MLExample.java
@@ -1,1 +1,12 @@
-package rainbownl
+package rainbownlp.machinelearning;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence
