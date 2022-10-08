e4fe260304f9b2c2dbc2b7d19cecac3d7ba3eb38
latinstallion <156602623+latinstallion@users.noreply.github.com>
2022-10-08 07:16:40 +0000

    Added some features

--- src/main/java/rainbownlp/machinelearning/MLExample.java
+++ src/main/java/rainbownlp/machinelearning/MLExample.java
@@ -9,4 +9,11 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.T
