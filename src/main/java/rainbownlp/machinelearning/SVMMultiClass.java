cf64a86990a10d70a8eee4653bbd1d30afde545d
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-01-16 13:13:40 +0000

    fix container

--- src/main/java/rainbownlp/machinelearning/SVMMultiClass.java
+++ src/main/java/rainbownlp/machinelearning/SVMMultiClass.java
@@ -3,4 +3,16 @@ package rainbownlp.machinelearning;
 import rainbownlp.util.ConfigurationUtil;
 
 public class SVMMultiClass extends SVMLightBasedLearnerEngine {
-	String mode
+	String modelFile;
+	String taskName;
+	String trainFile;
+	String testFile;
+	
+	private SVMMultiClass()
+	{
+		
+	}
+
+	public static LearnerEngine getLearnerEngine(String pTaskName) {
+		SVMMultiClass learnerEngine = new SVMMultiClass();
+		learnerEngine.setTaskName(pT
