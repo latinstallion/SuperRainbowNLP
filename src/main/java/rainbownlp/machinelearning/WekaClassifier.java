610be2ec3fed92ffcd0700a6a04b6aad9aca4add
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-01-31 13:54:53 +0000

    Update Italian translation.

--- src/main/java/rainbownlp/machinelearning/WekaClassifier.java
+++ src/main/java/rainbownlp/machinelearning/WekaClassifier.java
@@ -10,4 +10,14 @@ import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
-import weka.core.SerializationHelper
+import weka.core.SerializationHelper;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class WekaClassifier extends LearnerEngine {
+	String modelFile;
+	String taskName;
+	String trainFile;
+	String testFile;
+	
+	int reinforcedCount = 0;
+	String[] reinforcedModels = new String[reinforcedCo
