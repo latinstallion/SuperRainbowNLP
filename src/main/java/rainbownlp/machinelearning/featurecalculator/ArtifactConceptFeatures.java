dfd9989cd02353d386fd8d86d3019add8db740b6
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-02-15 10:44:59 +0000

    Setting version to 0.2-SNAPSHOT

--- src/main/java/rainbownlp/machinelearning/featurecalculator/ArtifactConceptFeatures.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/ArtifactConceptFeatures.java
@@ -1,1 +1,7 @@
-pack
+package rainbownlp.machinelearning.featurecalculator;
+
+public enum ArtifactConceptFeatures {
+	ConceptContent,
+	ArtifactContent, ConceptInArtifactContent, CountInDocument, CountInSentence, 
+	ConceptSectionType, SentenceWordSize,
+	NonNormalizedNGram
