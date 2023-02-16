e7153a5a8734828f6034f279cf851275721658f5
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-02-16 15:43:40 +0000

    v5

---
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/link/ConceptsDistance.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+package rainbownlp.machinelearning.featurecalculator.link;
+
+import java.util.List;
+import rainbownlp.machinelearning.IFeatureCalculator;
+import rainbownlp.machinelearn
