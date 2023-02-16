290d054359da0b0a8c9a7ab3246852b15d86ef9c
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-02-16 21:29:19 +0000

    Envío PHP

--- src/main/java/rainbownlp/machinelearning/featurecalculator/link/ConceptsDistance.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/link/ConceptsDistance.java
@@ -5,4 +5,15 @@ package rainbownlp.machinelearning.featurecalculator.link;
 
 import java.util.List;
 import rainbownlp.machinelearning.IFeatureCalculator;
-import rainbownlp.machinelearn
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.util.FileUtil;
+
+/**
+ * @author ehsan
+ * 
+ */
+public class ConceptsDistance implements IFeatureCalculator {
+	public static void main (String[] args) throws Exception
+	{
+		String experimentgroup = "LinkClassificationEventEvent";
+		List<MLExample> 
