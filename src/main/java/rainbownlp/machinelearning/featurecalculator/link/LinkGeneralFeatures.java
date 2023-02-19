f1e60ff681b9d99d27bf43ce04237a81a847ff91
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-02-19 08:39:11 +0000

    add footer

--- src/main/java/rainbownlp/machinelearning/featurecalculator/link/LinkGeneralFeatures.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/link/LinkGeneralFeatures.java
@@ -8,4 +8,14 @@ import java.util.List;
 import rainbownlp.core.FeatureValuePair;
 import rainbownlp.core.Phrase;
 import rainbownlp.core.PhraseLink;
-import rainbownlp.core.FeatureValuePair.Fea
+import rainbownlp.core.FeatureValuePair.FeatureName;
+import rainbownlp.machinelearning.IFeatureCalculator;
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.machinelearning.MLExampleFeature;
+import rainbownlp.util.FileUtil;
+
+/**
+ * @author Azadeh
+ * 
+ */
+public class LinkGeneralFeatures implements IFeatureCalcu
