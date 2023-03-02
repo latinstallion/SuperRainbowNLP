a2b966105c19024b8226bd6cad9346a4906a18dd
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-03-02 21:45:59 +0000

    new doc

--- src/main/java/rainbownlp/machinelearning/featurecalculator/link/ParseDependencyFeatures.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/link/ParseDependencyFeatures.java
@@ -4,4 +4,12 @@
 package rainbownlp.machinelearning.featurecalculator.link;
 
 import java.sql.SQLException;
-import java.util.ArrayList
+import java.util.ArrayList;
+import java.util.List;
+
+import rainbownlp.analyzer.sentenceclause.Clause;
+import rainbownlp.analyzer.sentenceclause.SentenceClauseManager;
+import rainbownlp.core.Artifact;
+import rainbownlp.core.FeatureValuePair;
+import rainbownlp.core.Phrase;
+import rai
