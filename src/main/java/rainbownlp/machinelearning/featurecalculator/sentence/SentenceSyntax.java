45ca41f35a82d33df245c4569e877a45048fd469
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-04-26 16:21:36 +0000

    kvm: don't take vcpu mutex for obviously invalid vcpu ioctls

--- src/main/java/rainbownlp/machinelearning/featurecalculator/sentence/SentenceSyntax.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/sentence/SentenceSyntax.java
@@ -4,4 +4,11 @@ import rainbownlp.core.Artifact;
 import rainbownlp.core.FeatureValuePair;
 import rainbownlp.core.Artifact.Type;
 import rainbownlp.machinelearning.IFeatureCalculator;
-import rai
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.machinelearning.MLExampleFeature;
+
+public class SentenceSyntax implements IFeatureCalculator {
+
+	@Override
+	public void calculateFeatures(MLExample exampleToProcess) {
+		Artifact sentence = examp
