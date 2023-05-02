393c673badca8684eb2782e38c5480f4c296af06
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-05-02 15:23:00 +0000

    Minor UI tweaks

--- src/main/java/rainbownlp/parser/DependencyLine.java
+++ src/main/java/rainbownlp/parser/DependencyLine.java
@@ -2,4 +2,14 @@ package rainbownlp.parser;
 
 public class DependencyLine {
 	public String relationName;
-	public String firstP
+	public String firstPart;
+	public String secondPart;
+	public int firstOffset;
+	public int secondOffset;
+	public boolean hasWord(String word) {
+		if(firstPart.equals(word) ||
+				secondPart.equals(word))
+			return true;
+		return false;
+	}
+}
