17f80ccebbba9865c48074f0488bcbfa9454bb4f
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-06-08 10:50:17 +0000

    Minor cosmetic fix

--- src/main/java/rainbownlp/preprocess/DocumentAnalyzer.java
+++ src/main/java/rainbownlp/preprocess/DocumentAnalyzer.java
@@ -2,4 +2,13 @@ package rainbownlp.preprocess;
 
 import java.util.List;
 
-import rain
+import rainbownlp.core.Artifact;
+import rainbownlp.core.RainbowEngine.DatasetType;
+
+public abstract class DocumentAnalyzer {
+	static public enum InputType {
+		TextFiles
+	}
+	/**
+	 * Process given documents and load them into Artifact table
+	 * @param rootPath root of al
