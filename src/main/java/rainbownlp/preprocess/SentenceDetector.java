58359d29e5a25c9fb993e85d3d4b0ebe506d2369
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-06-10 12:06:03 +0000

    Update README.jp.md

--- src/main/java/rainbownlp/preprocess/SentenceDetector.java
+++ src/main/java/rainbownlp/preprocess/SentenceDetector.java
@@ -1,4 +1,12 @@
 package rainbownlp.preprocess;
 
 import java.io.FileInputStream;
-import java
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.util.Span;
+import rainbownlp.util.Confi
