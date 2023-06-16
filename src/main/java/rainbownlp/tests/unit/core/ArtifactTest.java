e3aa1d015b8d083a3508e934e19f3a139a7bec05
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-06-16 18:39:22 +0000

    Major code refactoring

--- src/main/java/rainbownlp/tests/unit/core/ArtifactTest.java
+++ src/main/java/rainbownlp/tests/unit/core/ArtifactTest.java
@@ -6,4 +6,15 @@ import org.junit.Test;
 
 import rainbownlp.core.Artifact;
 import rainbownlp.core.Artifact.Type;
-import
+import rainbownlp.util.HibernateUtil;
+
+public class ArtifactTest   {
+	
+	@Test
+	public void testCreateArtifact() {
+		Artifact doc_artifact = Artifact.getInstance(Type.Document);
+		assertNotNull(doc_artifact);
+
+		doc_artifact.setContent( "this is test. hello test.");
+		
+		HibernateUtil.save(doc_artifact);
