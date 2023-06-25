0e08267e9f8482d08be2073ef5f2065deee845a2
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-06-25 11:26:35 +0000

    Many updates

--- src/main/java/rainbownlp/tests/unit/core/MLExampleFeatureTest.java
+++ src/main/java/rainbownlp/tests/unit/core/MLExampleFeatureTest.java
@@ -7,4 +7,13 @@ import org.junit.Test;
 import rainbownlp.core.Artifact;
 import rainbownlp.core.Artifact.Type;
 import rainbownlp.core.FeatureValuePair.FeatureName;
-import rainbownlp.core.F
+import rainbownlp.core.FeatureValuePair;
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.machinelearning.MLExampleFeature;
+import rainbownlp.util.HibernateUtil;
+
+public class MLExampleFeatureTest   {
+	
+	@Test
+	public void testCreateArtifact() {
+		Artifact doc_artifact = Artifac
