ea99615dbf81dafa6bcad469a212d67dda9eee14
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-07-01 17:32:52 +0000

    Updating License

--- src/main/java/rainbownlp/util/ExtensionFilter.java
+++ src/main/java/rainbownlp/util/ExtensionFilter.java
@@ -1,4 +1,14 @@
 package rainbownlp.util;
 
 import java.io.File;
-import java.io.FileFilter
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+
+public class ExtensionFilter implements FilenameFilter, FileFilter {
+	  private String extension;
+	  public ExtensionFilter( String extension ) {
+	    this.extension = extension;             
+	  }
+	  
+	  @Override
+	public boolea
