5191bea794a6f85e8f0fcb43dbc44fee5676b815
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-07-03 22:40:08 +0000

    debug

--- src/main/java/rainbownlp/util/PorterStemmer.java
+++ src/main/java/rainbownlp/util/PorterStemmer.java
@@ -1,1 +1,17 @@
-package rai
+package rainbownlp.util;
+
+
+/*
+
+   Porter stemmer in Java. The original paper is in
+
+       Porter, 1980, An algorithm for suffix stripping, Program, Vol. 14,
+       no. 3, pp 130-137,
+
+   See also http://www.tartarus.org/~martin/PorterStemmer
+
+   History:
+
+   Release 1
+
+   B
