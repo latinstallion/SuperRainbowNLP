a08fc7440456175f0c4b812bf7af14ca8630ce46
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-07-05 14:28:51 +0000

    blkcg: don't call into policy draining if root_blkg is already gone

--- src/main/java/rainbownlp/util/PorterStemmer.java
+++ src/main/java/rainbownlp/util/PorterStemmer.java
@@ -14,4 +14,12 @@ package rainbownlp.util;
 
    Release 1
 
-   B
+   Bug 1 (reported by Gonzalo Parra 16/10/99) fixed as marked below.
+   The words 'aed', 'eed', 'oed' leave k at 'a' for step 3, and b[k-1]
+   is then out outside the bounds of b.
+
+   Release 2
+
+   Similarly,
+
+   Bug 2 (reported by Steve Dyrdahl 22/2/00) fixed as marke
