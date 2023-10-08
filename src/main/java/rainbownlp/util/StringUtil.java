a1c1e969f93446defc5dc591ac4080ee7523aa26
latinstallion <156602623+latinstallion@users.noreply.github.com>
2023-10-08 18:46:21 +0000

    todo notes

--- src/main/java/rainbownlp/util/StringUtil.java
+++ src/main/java/rainbownlp/util/StringUtil.java
@@ -4,4 +4,10 @@ import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.uti
+import java.util.HashMap;
+import java.util.Properties;
+
+import weka.core.Stopwords;
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations
